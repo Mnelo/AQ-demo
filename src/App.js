33d3bf934fd523293365a926a2df6740433d0734
hy918 <23854996732qq.com>
2021-12-09 10:32:06 +0800

    侧边栏

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Head from "./Component/Head";
+import SideBar from "./Component/SideBar"
 import "./App.css";
-
+import 'antd/dist/antd.min.css';
 const App = () => {
   return (
     <div className="App">
       <Head />
+      <SideBar />
     </div>
   );
 };
---
+++ src/Component/IconFont/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createFromIconfontCN } from '@ant-design/icons';
+import iconfont from '../../Asset/font/iconfont.js'
+
+const SideBarIconFont = createFromIconfontCN({
+    scriptUrl: iconfont
+})
+
+export default SideBarIconFont;
